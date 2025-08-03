8f4e7116f2db758a21fe8b9a9b81e333ba3955ec
Mansi Tibude <112980762+MansiTibude9@users.noreply.github.com>
2025-08-03 17:25:17 +0530

    Add files via upload

---
+++ VehicleOperation.java
@@ -0,0 +1,129 @@
+//contructors, setter, getters, interfaces, classes, methods, functions
+/This code is not complete and will be somehow incorrect 
+class VehicleOperation
+{
+public static void main(String[] args)
+{
+private int noOfgears;
+private float speed;
+private boolean hybrid, nonhybrid;
+public boolean automatic;
+private string engineType;
+private string fuelType;
+private string tubeType;
+private float current speed;
+private string vehicleType;
+const float maxSpeed;
+const string vehType1 = electric;
+const string vehType2 = fuel;
+const string tubeType1 = tubeless;
+const string tubeType2 = tubed;
+private string static igni1 = manual;
+private string static igni2 = automatic;
+const string hospower;
+private static float max_speed = 360;
+private string modelNumber;
+private string modelType;
+private string modelName;
+private long priceValue;
+private string ignitionType;
+void setNoOfGears(int noOfGears)
+{
+this.noOfGears = noOfGears;
+}
+int getNoOfGears()
+{
+return noOfGears;
+}
+void setSpeed(float speed)
+{
+this.speed = speed;
+}
+float getSpeed()
+{
+return speed;
+}
+void setCheckHybrid(boolean hybrid,boolean nonHybrid)
+{
+if(hybrid == true)
+{
+this.hybrid = hybrid;
+}
+if(nonHybrid == true)
+{
+this.nonHybrid = nonHybrid;
+}
+void setAutomatic(boolean automatic)
+{
+this.automatic=automatic;
+}
+boolean getAutomatic()
+{
+return automatic;
+}
+void setEngineType(string engine)
+{
+this.engine = engine;
+}
+String getEngineType()
+{
+return engine;
+}
+void setFuelType(string fuelType)
+{
+this.fuelType = fuelType;
+}
+string getFuelType()
+{
+return fuelType;
+}
+void setTubeType(string tubeType)
+{
+this.tubeType = tubeType;
+}
+string getTubeType()
+{
+return tubeType;
+}
+void setVehicleType(string vehicleTye)
+{
+this.vehicleType = vehicleType;
+}
+int getVehicleType()
+{
+return vehicleType;
+}
+void setModelNumber(string modelNumber)
+{
+this.modelNumber = modelNumber;
+}
+string getModelNumber()
+{
+return modelNumber;
+}
+void setModelName(string modelName)
+{
+this.modelName;
+}
+string getModelName()
+{
+return this.modelName;
+}
+void setPriceValue(int priceValue)
+{
+this.priceValue = priceValue;
+}
+long getPriceValue()
+{
+return priceValue;
+}
+void setIgnitionType(string ignitionType)
+{
+this.ignitionType = ingnitionType;
+}
+string getIgnitionType()
+{
+return ignitionType;
+}
+}
+}
