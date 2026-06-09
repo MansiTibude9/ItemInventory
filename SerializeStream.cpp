d92cd18612d3f9257f42aef9e60fef4bf727b125
Mansi Tibude <112980762+MansiTibude9@users.noreply.github.com>
2026-06-09 17:59:57 +0530

    Delete SerializeStream.cpp

--- SerializeStream.cpp
+++
@@ -1,9 +0,0 @@
-class SerializeStream
-{
-int base;
-int size;
-void setBase(base){this.base = base;}
-int getBase(){return this.base;}
-void setSize(size){this.size = size;}
-set getSize(){return this.size;}
-}
