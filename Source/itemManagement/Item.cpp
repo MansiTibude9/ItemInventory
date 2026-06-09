5a5283f94f1f3c4980d5f9758c7e44f474837003
Mansi Tibude <112980762+MansiTibude9@users.noreply.github.com>
2026-06-09 17:54:31 +0530

    Adding Item Management Files

---
+++ Source/itemManagement/Item.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+class Item
+{
+int code;
+string name;
+void setCode(code)
+{
+this.code = code;
+}
+int getCode()
+{
+return this.code;
+}
+void setName(name)
+{
+this.name = name;
+}
+string getName()
+{
+return this.name;
+}
+}
+//serialize and deserialise data
+//dispose
