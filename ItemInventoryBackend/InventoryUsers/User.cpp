acf3d46e1d6e6fab977c8def92eed5d8a58b4dd9
Mansi Tibude <112980762+MansiTibude9@users.noreply.github.com>
2026-09-18 20:39:14 +0530

    Update User.cpp

--- ItemInventoryBackend/InventoryUsers/User.cpp
+++ ItemInventoryBackend/InventoryUsers/User.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
 
+class User
+{
+string name;
+int customerId;
+int age;
+char gender[3] = {'F','M','O'};
+bool loyalMember;
+};
+int main()
+{
+return 0;
+}
