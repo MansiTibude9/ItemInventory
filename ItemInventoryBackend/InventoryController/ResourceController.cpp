04a10440b6e1a6b92caf9b3a991babda1b2d149a
Mansi Tibude <112980762+MansiTibude9@users.noreply.github.com>
2026-09-19 16:53:53 +0530

    Update ResourceController.cpp

--- ItemInventoryBackend/InventoryController/ResourceController.cpp
+++ ItemInventoryBackend/InventoryController/ResourceController.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+class InventoryController
+{
+// Property - Name, ModelNo, Manufactuing Date, Ram, Rom,
+// Event Resource - Unicast, Connecting, Disconnecting - Port 
+// Methods - Get, Set, and Editing 
+// Event Types - Name, Events, eventInfo - Trigger for deployment 
+// Event Sink - Allocated memory release - after triggering the event 
+}
+int main()
+{
+  return 0;
+}
